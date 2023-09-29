9e21285383579fdd80c339c9f4c826c3475eed9f
Igor Fidler <igorgfidler@hotmail.com>
2023-09-29 10:49:57 -0300

    Change operation names

--- src/main/java/request/RequisitionOperations.java
+++ src/main/java/request/RequisitionOperations.java
@@ -3,6 +3,6 @@ package request;
 public interface RequisitionOperations {
     String LOGIN = "LOGIN";
     String LOGOUT = "LOGOUT";
-    String BUSCAR_USUARIOS = "BUSCAR_USUARIOS";
+    String LISTAR_USUARIOS = "LISTAR_USUARIOS";
     String BUSCAR_USUARIO = "BUSCAR_USUARIO";
 }
