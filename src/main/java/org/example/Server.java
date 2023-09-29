40f5a8bdf273e754483d975f51622fcf4129248e
Igor Fidler <igorgfidler@hotmail.com>
2023-09-29 10:49:31 -0300

    Refactor server main loop to use try with resources

--- src/main/java/org/example/Server.java
+++ src/main/java/org/example/Server.java
@@ -28,31 +28,20 @@ public class Server extends Thread {
     }
 
     public static void main(String[] args) {
-        ServerSocket serverSocket = null;
-
-        try {
-            serverSocket = new ServerSocket(24800);
+        try (ServerSocket serverSocket = new ServerSocket(24800)) {
             System.out.println("Connection Socket Created");
-            try {
-                while (true) {
+            while (true) {
+                try {
                     System.out.println("Waiting for Connection");
                     new Server(serverSocket.accept());
+                } catch (IOException e) {
+                    System.err.println("Accept failed.");
+                    System.exit(1);
                 }
-            } catch (IOException e) {
-                System.err.println("Accept failed.");
-                System.exit(1);
             }
         } catch (IOException e) {
             System.err.println("Could not listen on port: 10008.");
             System.exit(1);
-        } finally {
-            try {
-                assert serverSocket != null;
-                serverSocket.close();
-            } catch (IOException e) {
-                System.err.println("Could not close port: 10008.");
-                System.exit(1);
-            }
         }
     }
 
