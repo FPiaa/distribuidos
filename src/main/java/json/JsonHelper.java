fb13c7c55ef5f7dfcda0788b69e4ef2b863c078d
Igor Fidler <igorgfidler@hotmail.com>
2023-09-29 22:50:13 -0300

    Remove unnecessary factory's in json helper

--- src/main/java/json/JsonHelper.java
+++ src/main/java/json/JsonHelper.java
@@ -2,14 +2,10 @@ package json;
 
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
-import json.adapter.ConvertNullToEmptyAdapterFactory;
-import json.adapter.NonOptionalTypeAdapterFactory;
-import json.adapter.OptionalTypeAdapterFactory;
+import json.adapter.CheckOptionalTypeAdapterFactory;
 
 public interface JsonHelper {
     Gson gson = new GsonBuilder()
-            .registerTypeAdapterFactory(NonOptionalTypeAdapterFactory.FACTORY)
-            .registerTypeAdapterFactory(OptionalTypeAdapterFactory.FACTORY)
-            .registerTypeAdapterFactory(ConvertNullToEmptyAdapterFactory.FACTORY)
+            .registerTypeAdapterFactory(CheckOptionalTypeAdapterFactory.FACTORY)
             .create();
 }
