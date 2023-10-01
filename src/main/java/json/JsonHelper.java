d3df2b86bb31cb2959ac0bc1e429b819cda45a77
Igor Fidler <igorgfidler@hotmail.com>
2023-10-01 20:47:16 -0300

    Create a better json helper

--- src/main/java/json/JsonHelper.java
+++ src/main/java/json/JsonHelper.java
@@ -2,10 +2,28 @@ package json;
 
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
+import com.google.gson.JsonSyntaxException;
 import json.adapter.CheckOptionalTypeAdapterFactory;
+import org.checkerframework.checker.nullness.qual.NonNull;
+import server.exceptions.BadRequestException;
+import server.exceptions.ServerResponseException;
 
-public interface JsonHelper {
-    Gson gson = new GsonBuilder()
+public class JsonHelper {
+    private static final Gson gson = new GsonBuilder()
             .registerTypeAdapterFactory(CheckOptionalTypeAdapterFactory.FACTORY)
             .create();
+
+    public static <T> T fromJson(@NonNull final String json, Class<T> returnType) throws ServerResponseException {
+        try {
+            return gson.fromJson(json, returnType);
+        } catch (JsonSyntaxException ex) {
+            throw new BadRequestException();
+        }
+    }
+
+    public static <T> String toJson(T object) {
+        return gson.toJson(object);
+    }
+
+
 }
