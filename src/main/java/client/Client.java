c76fc575754c745c1691bc6ab8b531fd66af0684
Igor Fidler <igorgfidler@hotmail.com>
2023-10-01 21:19:33 -0300

    Refactor Client

--- src/main/java/client/Client.java
+++ src/main/java/client/Client.java
@@ -20,19 +20,16 @@ import java.net.UnknownHostException;
 
 public class Client {
     public static void main(String[] args) throws IOException {
-        BufferedReader stdin = new BufferedReader(new InputStreamReader(System.in));
         String serverHost = "localhost";
         int port = 24800;
 
-        Socket echoSocket = null;
-        PrintWriter out = null;
-        BufferedReader in = null;
-
-        try {
-            echoSocket = new Socket(serverHost, port);
-            out = new PrintWriter(echoSocket.getOutputStream(), true);
-            in = new BufferedReader(new InputStreamReader(
-                    echoSocket.getInputStream()));
+        try (Socket echoSocket = new Socket(serverHost, port);
+             PrintWriter out = new PrintWriter(echoSocket.getOutputStream(), true);
+             BufferedReader in = new BufferedReader(new InputStreamReader(
+                     echoSocket.getInputStream()));
+             BufferedReader stdin = new BufferedReader(new InputStreamReader(System.in));
+        ) {
+            repl(echoSocket, out, in, stdin);
         } catch (UnknownHostException e) {
             System.err.println("Don't know about host: " + serverHost);
             System.exit(1);
@@ -41,44 +38,45 @@ public class Client {
                     + "the connection to: " + serverHost);
             System.exit(1);
         }
+    }
 
+    private static void repl(Socket server, PrintWriter out, BufferedReader in, BufferedReader stdin) {
         String token = "";
         String userInput;
-        while ((userInput = stdin.readLine()) != null) {
-            if (userInput.equals("Bye.")) {
-                var logoutRequest = new LogoutRequest(token);
-                String request = JsonHelper.toJson(logoutRequest);
+        try {
+            while ((userInput = stdin.readLine()) != null) {
+                if (userInput.equals("Bye.")) {
+                    var logoutRequest = new LogoutRequest(token);
+                    String request = JsonHelper.toJson(logoutRequest);
+                    out.println(request);
+
+                    System.out.println("Enviado: " + request);
+                    String response = in.readLine();
+                    System.out.println("Recebido: " + response);
+                    break;
+                }
+                LoginRequest login = new LoginRequest("email", "senah");
+                String request = JsonHelper.toJson(login);
                 out.println(request);
+
 
                 System.out.println("Enviado: " + request);
                 String response = in.readLine();
                 System.out.println("Recebido: " + response);
-                break;
+                try {
+                    var loginResponse = JsonHelper.fromJson(response, LoginResponse.class);
+                    token = loginResponse.payload().token();
+                    DecodedJWT decodedJWT = JWT.decode(token);
+                    int userId = decodedJWT.getClaim("userId").asInt();
+                    System.out.println("User id: " + userId);
+                } catch (JsonSyntaxException e) {
+                    ErrorResponse error = new Gson().fromJson(response, ErrorResponse.class);
+                    System.out.println(error.toString());
+                }
             }
-            LoginRequest login = new LoginRequest("email", "senah");
-            String request = JsonHelper.toJson(login);
-            out.println(request);
-
-
-            System.out.println("Enviado: " + request);
-            String response = in.readLine();
-            System.out.println("Recebido: " + response);
-            try {
-                var loginResponse = new Gson().fromJson(response, LoginResponse.class);
-                token = loginResponse.payload().token();
-                DecodedJWT decodedJWT = JWT.decode(token);
-                int userId = decodedJWT.getClaim("userId").asInt();
-                System.out.println("User id: " + userId);
-            } catch (JsonSyntaxException e) {
-                ErrorResponse error = new Gson().fromJson(response, ErrorResponse.class);
-                System.out.println(error.toString());
-            }
+        } catch (IOException e) {
+            System.out.println("error reading stdin");
         }
-
-        out.close();
-        in.close();
-        stdin.close();
-        echoSocket.close();
     }
 }
 
