3c858cdff36f31e3314e77bd308cdffd06b9777e
Igor Fidler <igorgfidler@hotmail.com>
2023-10-03 19:30:34 -0300

    Add tests for router

---
+++ src/test/java/server/router/RouterTest.java
@@ -0,0 +1,105 @@
+package server.router;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+import protocol.response.Response;
+import server.exceptions.BadRequestException;
+import server.exceptions.MethodNotAllowedException;
+import server.exceptions.ServerResponseException;
+import server.layer.interfaces.InitialLayer;
+
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+public class RouterTest {
+
+    private Router router;
+
+    @BeforeEach
+    public void createRouter() {
+        router = Router.builder()
+                .addRoute("OP1", new InitialLayer() {
+                    @Override
+                    public Response<?> startService(String jsonString) throws ServerResponseException {
+                        return null;
+                    }
+                })
+                .build();
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"""
+            {"header": {"operation":"OP1"}}
+            """,
+            """
+                    {"header": {"operation":"OP1", "token": "token"}}
+                    """,
+            """
+                    {"header": {"operation":"OP1", "token": "token"}, "payload": null}
+                    """,
+            """
+                    {"header": {"operation":"OP1", "token": "token"}, "payload": {"some": 123, "object": "foo"}}
+                    """
+    })
+    public void GivenValidRequestHeader_whenServe_getsResponse(String json) {
+        assertDoesNotThrow(() -> router.serve(json));
+    }
+
+    @ParameterizedTest()
+    @ValueSource(strings = {
+            """
+                    {"header": {"operation": 1}}""",
+            """
+                    {"header": {"operation":"OP1", "token": 123}}""",
+            """
+                    {"header": {"operation": 1.0, "token": 123.1}}""",
+            """
+                    {"header": {"operation":"OP1", "token": 123}}"""
+    })
+    public void GivenWrongTypeInHeader_whenServe_getsWrongTypeErrorResponse(String json) {
+        assertThrows(BadRequestException.class, () -> router.serve(json));
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {
+            """
+                    {"header": {"operation":null}}
+                    """,
+
+            """
+                    {"header": {"operation":""}}
+                    """,
+            """
+                    {"header": {"token": "token"}}
+                    """,
+            """
+                    {"header": {}}
+                    """,
+    })
+    public void GivenHeaderMissingOperation_whenServe_getsMissingObligatoryFieldsResponse(String json) {
+        assertThrows(MethodNotAllowedException.class, () -> router.serve(json));
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {
+            """
+                    {"header": {"operation":"OP2"}}
+                    """,
+            """
+                    {"header": {"operation":"OP2", "token": "token"}}
+                    """,
+            """
+                    {"header": {"operation":"OP2", "token": "token"}, "payload": null}
+                    """,
+            """
+                    {"header": {"operation":"OP2", "token": "token"}, "payload": {"some": 123, "object": "foo"}}
+                    """
+    })
+    public void GivenValidHeaderWithoutKnownOperation_whenServe_getsMethodNotAllowedErrorResponse(String json) {
+        assertThrows(MethodNotAllowedException.class, () -> router.serve(json));
+
+    }
+
+
+}
