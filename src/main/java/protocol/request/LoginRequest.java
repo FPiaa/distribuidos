06915b90a3723e9799b4dd5ca5026c459f601dee
Igor Fidler <igorgfidler@hotmail.com>
2023-10-03 21:33:31 -0300

    Add constraints on LoginRequest

--- src/main/java/protocol/request/LoginRequest.java
+++ src/main/java/protocol/request/LoginRequest.java
@@ -1,18 +1,19 @@
 package protocol.request;
 
 import jakarta.validation.Valid;
+import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.NotNull;
 import protocol.request.header.Header;
 
 public record LoginRequest(@NotNull @Valid Header header,
-                           LoginRequest.@NotNull @Valid Payload payload) implements Request<LoginRequest.Payload> {
+                           @NotNull @Valid Payload payload) implements Request<LoginRequest.Payload> {
 
     public LoginRequest(final @NotEmpty String email, final @NotEmpty String password) {
         this(new Header(RequisitionOperations.LOGIN, null), new Payload(email, password));
     }
 
-    public record Payload(@NotEmpty String email, @NotEmpty String password) {
+    public record Payload(@NotEmpty @Email String email, @NotEmpty String password) {
     }
 }
 
