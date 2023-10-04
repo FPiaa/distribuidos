75a4404dc792d5a8d1b50514a3ea9da9f82923c7
Igor Fidler <igorgfidler@hotmail.com>
2023-10-03 23:32:32 -0300

    Fix JwtHelper

--- src/main/java/jwt/JwtHelper.java
+++ src/main/java/jwt/JwtHelper.java
@@ -3,6 +3,7 @@ package jwt;
 import com.auth0.jwt.JWT;
 import com.auth0.jwt.JWTVerifier;
 import com.auth0.jwt.algorithms.Algorithm;
+import com.auth0.jwt.exceptions.JWTVerificationException;
 import com.auth0.jwt.interfaces.DecodedJWT;
 
 public class JwtHelper {
@@ -10,12 +11,12 @@ public class JwtHelper {
         Algorithm alg = Algorithm.HMAC256("hiuhi");
         return JWT.create()
                 .withIssuer("me")
-                .withClaim("isAdmin", false)
-                .withClaim("userId", 1)
+                .withClaim("isAdmin", isAdmin)
+                .withClaim("userId", userId)
                 .sign(alg);
     }
 
-    public static DecodedJWT verify(String token) {
+    public static DecodedJWT verify(String token) throws JWTVerificationException {
         JWTVerifier verifier = JWT.require(Algorithm.HMAC256("hiuhi")).build();
         return verifier.verify(token);
     }
