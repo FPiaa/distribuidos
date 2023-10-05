8702fde0a65fac6f8834b7c395a7c90ae3fbfb72
Igor Fidler <igorgfidler@hotmail.com>
2023-10-05 00:23:04 -0300

    Create AdminCreateUserRequest

---
+++ src/main/java/protocol/request/AdminCreateUserRequest.java
@@ -0,0 +1,37 @@
+package protocol.request;
+
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import lombok.Getter;
+import protocol.request.header.Header;
+
+@Getter
+public class AdminCreateUserRequest extends Request<AdminCreateUserRequest.Payload> {
+
+    @NotNull(message = "payload não pode ser nulo")
+    @Valid
+    private final Payload payload;
+
+    public AdminCreateUserRequest(final String token, final String nome, final String email,
+                                  final String senha, final Boolean tipo) {
+        super(new Header(RequisitionOperations.ADMIN_CADASTRAR_USUARIO, token));
+        payload = new Payload(nome, email, senha, tipo);
+    }
+
+    public record Payload(
+            @NotBlank(message = "nome não pode estar vazio")
+            @Size(min = 3, max = 255, message = "nome deve ter entre 3 e 255 caracteres")
+            String nome,
+            @NotBlank(message = "email não pode estar vazio")
+            @Email(message = "email deve conter um email valido")
+            String email,
+            @NotBlank(message = "senha não pode estar vazio")
+            String senha,
+            @NotNull(message = "tipo não pode ser nulo")
+            Boolean tipo
+    ) {
+    }
+}
--- src/main/java/protocol/request/RequisitionOperations.java
+++ src/main/java/protocol/request/RequisitionOperations.java
@@ -5,4 +5,5 @@ public interface RequisitionOperations {
     String LOGOUT = "LOGOUT";
     String ADMIN_BUSCAR_USUARIOS = "ADMIN_BUSCAR_USUARIOS";
     String ADMIN_BUSCAR_USUARIO = "ADMIN_BUSCAR_USUARIO";
+    String ADMIN_CADASTRAR_USUARIO = "ADMIN_CADASTRAR_USUARIO";
 }
