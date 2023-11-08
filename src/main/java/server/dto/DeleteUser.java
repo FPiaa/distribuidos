8ab2441aad5dbe73ea2a3532a9c71f0836b4ab24
Igor Fidler <igorgfidler@hotmail.com>
2023-11-07 23:52:34 -0300

    Add new requests kinds

--- src/main/java/server/dto/DeleteUser.java
+++ src/main/java/server/dto/DeleteUser.java
@@ -5,6 +5,5 @@ import lombok.NonNull;
 
 @Builder
 public record DeleteUser(@NonNull Long registroSender, @NonNull Boolean isSenderAdmin,
-                         @NonNull Long registroToDelete,
-                         String email, String senha) {
+                         @NonNull Long registroToDelete) {
 }
