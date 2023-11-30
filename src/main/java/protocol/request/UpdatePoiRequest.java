34e0d5677f80154b2e30f71337ee7b7e9c77fdb8
Igor Fidler <igorgfidler@hotmail.com>
2023-11-30 11:19:11 -0300

    a

--- src/main/java/protocol/request/UpdatePoiRequest.java
+++ src/main/java/protocol/request/UpdatePoiRequest.java
@@ -13,15 +13,15 @@ public class UpdatePoiRequest extends Request<UpdatePoiRequest.Payload> {
     @NotNull
     private final Payload payload;
 
-    public UpdatePoiRequest(String token,Long id, @Optional String name, @Optional Integer x, @Optional Integer y, @Optional Integer z, @Optional String aviso, @Optional Boolean acessivel) {
+    public UpdatePoiRequest(String token, Long id, @Optional String name, @Optional Integer x, @Optional Integer y, @Optional Integer z, @Optional String aviso, @Optional Boolean acessivel) {
         super(new Header(RequisitionOperations.ATUALIZAR_PDI, token));
-        if(x == null && y == null && z == null){
-            this.payload = new Payload(id,name, null, aviso, acessivel);
-        }
-        else {
-            this.payload = new Payload(id,name, new Position(x, y, z), aviso, acessivel);
+        if (x == null && y == null && z == null) {
+            this.payload = new Payload(id, name, null, aviso, acessivel);
+        } else {
+            this.payload = new Payload(id, name, new Position(x == null ? 0 : x, y == null ? 0 : y, z == null ? 0 : z), aviso, acessivel);
         }
     }
 
-    public record Payload(@NotNull Long id, String nome, @Valid Position posicao, String aviso, Boolean acessivel) {}
+    public record Payload(@NotNull Long id, String nome, @Valid Position posicao, String aviso, Boolean acessivel) {
+    }
 }
