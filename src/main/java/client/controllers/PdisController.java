f14acdf72ab0ec00d0c5d7d59b48b2fd40e03fe0
Igor Fidler <igorgfidler@hotmail.com>
2023-12-06 00:14:42 -0300

    tela de usuário tá show de bola

---
+++ src/main/java/client/controllers/PdisController.java
@@ -0,0 +1,60 @@
+package client.controllers;
+
+import io.github.palexdev.materialfx.controls.MFXButton;
+import io.github.palexdev.materialfx.controls.MFXCheckbox;
+import io.github.palexdev.materialfx.controls.MFXTextField;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+
+public class PdisController {
+
+    @FXML
+    private MFXButton deleteButton;
+
+    @FXML
+    private MFXCheckbox isAcessibleCheck;
+
+    @FXML
+    private MFXButton modifyButton;
+
+    @FXML
+    private MFXTextField nameField;
+
+    @FXML
+    private MFXButton novoPdi;
+
+    @FXML
+    private MFXTextField warningField;
+
+    @FXML
+    private MFXTextField xField;
+
+    @FXML
+    private MFXTextField yField;
+
+    @FXML
+    void cancel(ActionEvent event) {
+
+    }
+
+    @FXML
+    void createPdi(ActionEvent event) {
+
+    }
+
+    @FXML
+    void deletePdi(ActionEvent event) {
+
+    }
+
+    @FXML
+    void formCreatePdi(ActionEvent event) {
+
+    }
+
+    @FXML
+    void refreshTable(ActionEvent event) {
+
+    }
+
+}
